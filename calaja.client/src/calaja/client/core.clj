3a11d1092ceca6b6f37972481039558fc7d43f53
Michael <michael.stockli@videoplaza.com>
2012-03-30 10:16:31 +0200

    start project

---
+++ calaja.client/src/calaja/client/core.clj
@@ -0,0 +1,1 @@
+(ns calaja.client.core)
---
+++ calaja.client/test/calaja/client/test/core.clj
@@ -0,0 +1,6 @@
+(ns calaja.client.test.core
+  (:use [calaja.client.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
