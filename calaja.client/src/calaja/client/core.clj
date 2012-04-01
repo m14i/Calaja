9e70a006a3d313a713d911e6d480df9991eb5f4f
michael <michael.stoeckli@gmail.com>
2012-04-01 18:19:18 +0200

    removed Panel, only using Frame

--- calaja.client/src/calaja/client/core.clj
+++ calaja.client/src/calaja/client/core.clj
@@ -1,15 +1,15 @@
 (ns calaja.client.core
-  (:import (javax.swing JFrame JPanel)
-           (java.awt RenderingHints Color BasicStroke)
-           (java.awt.geom Ellipse2D AffineTransform Path2D)
-           (java.awt.event KeyListener KeyEvent)))
+  (:import (java.awt RenderingHints Color BasicStroke Toolkit)
+           (java.awt.event KeyListener KeyEvent)
+           (java.awt.geom AffineTransform Path2D)
+           (javax.swing JFrame)))
 
 (def _height 800)
 (def _width 800)
 (def _delay 20)
 (def _keys-held (atom #{}))
-(def _origin (doto (AffineTransform.) (.translate (/ _width 2) (/ _height 2)) (.scale 1 -1)))
 (def _hints (RenderingHints. RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON))
+(def _center [(/ _width 2) (/ _height 2)])
 
 (defn transform [shape loc rot]
   (let [[x y] loc]
@@ -21,6 +21,11 @@
 (defprotocol ISprite
   (draw [this g]))
 
+(defrecord Bullet [shape owner loc vel]
+  ISprite
+  (draw [this g]
+    (.draw g (transform (:shape this) (:loc this) 0))))
+
 (defrecord Player [shape name lives shield loc rot vel]
   ISprite
   (draw [this g]
@@ -31,26 +36,27 @@
         [x0 y0] (map first points)
         lines (rest points)
         path (java.awt.geom.Path2D$Double.)]
-    (do
-      (.moveTo path x0 y0)
-      (doseq [[xn yn] lines] (.lineTo path xn yn))
-      path)))
+    (.moveTo path x0 y0)
+    (doseq [[xn yn] lines] (.lineTo path xn yn))
+    (.closePath path)
+    path))
 
 (defn mk-ship [size]
   (letfn [(scale [x] (->> 2 Math/sqrt (/ 1) (* size x)))]
     (mk-path
-      (map scale [0 3 2 1 0 -1 -2 -3 0])
-      (map scale [3 0 -1 0 -1 0 -1 0 3]))))
+      (map scale [0 3 2 1 0 -1 -2 -3])
+      (map scale [3 0 -1 0 -1 0 -1 0]))))
 
-(def _player (atom (Player. (mk-ship 20) "player" 3 3 [0 0] 0 [0 0])))
+(def _player (atom (Player. (mk-ship 20) "player" 3 3 _center 0 [0 0])))
+(def _bullets (atom []))
 
 (defn render [g]
   (draw @_player g))
 
 (defn rotate-by [keys]
   (cond
-    (keys KeyEvent/VK_LEFT) 0.1
-    (keys KeyEvent/VK_RIGHT) -0.1
+    (keys KeyEvent/VK_LEFT) -0.1
+    (keys KeyEvent/VK_RIGHT) 0.1
     :else 0))
 
 (defn accelerate-by [keys]
@@ -83,13 +89,11 @@
   (System/currentTimeMillis))
 
 (defn mk-canvas []
-  (proxy [JPanel KeyListener Runnable] []
-    (paintComponent [g]
-      (proxy-super paintComponent g)
-      (do
-        (.setTransform g _origin)
-        (.setRenderingHints g _hints)
-        (render g)))
+  (proxy [JFrame KeyListener Runnable] []
+    (paint [g]
+      (proxy-super paint g)
+      (.setRenderingHints g _hints)
+      (render g))
     (keyTyped [_])
     (keyReleased [ev]
       (swap! _keys-held disj (.getKeyCode ev)))
@@ -102,24 +106,21 @@
       (loop [ti (now)]
         (let [tj (now)
               dt (- tj ti)]
-          (do
-            (step dt)
-            (.repaint this)
-            (let [elapsed (- (now) ti)
-                  sleep (max 2 (- _delay elapsed))]
-              (Thread/sleep sleep)
-              (recur tj))))))))
+          (step dt)
+          (.repaint this)
+          (let [elapsed (- (now) ti)
+                sleep (max 2 (- _delay elapsed))]
+            (Thread/sleep sleep)
+            (recur tj)))))))
 
 (defn show []
-  (let [canvas (mk-canvas)
-        frame (JFrame. "Calaja")]
+  (let [canvas (mk-canvas)]
     (doto canvas
       (.setFocusable true)
-      (.addKeyListener canvas))
-    (doto frame
-      (.setContentPane canvas)
+      (.addKeyListener canvas)
       (.setSize _width _height)
-      (.setVisible true))))
+      (.setVisible true)
+      (.createBufferStrategy 2))))
 
 (defn -main [& args]
   (show))
